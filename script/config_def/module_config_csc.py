a844e46e49e7cce66fbb08f790b29b0314f19d28
vance <vance.wu@rock-chips.com>
2025-07-17 14:31:11 +0800

    增加csc配置抽象实现

---
+++ script/config_def/module_config_csc.py
@@ -0,0 +1,165 @@
+"""
+Copyright   : Copyright (c) 2025 by Rockchip. All right reserved.
+FilePath    : module_config_csc.py
+Author      : vance.wu@rock-chips.com
+Date        : 2025-07-16
+Description :
+LastEditTime: 2025-07-16
+"""
+
+import os
+import sys
+import json
+import random
+import numpy as np
+
+sys.path.append(os.path.normpath(os.path.dirname(__file__) + "/../"))
+from config_def.module_config_core import *
+
+
+class CscConfig(ModuleConfigCore):
+    def __init__(self, name: str = "CSC", version: str = "unknown"):
+        super().__init__(name, version)
+
+        self.cscEnable = 1
+        self.cscCctCtrlEn = 0
+        self.cscBrightness = 256
+        self.cscHue = 256
+        self.cscContrast = 256
+        self.cscSaturation = 256
+        self.cscRGain = 256
+        self.cscGGain = 256
+        self.cscBGain = 256
+        self.cscROffset = 256
+        self.cscGOffset = 256
+        self.cscBOffset = 256
+        self.cscMatrix = np.identity(3, dtype=np.int16)
+        self.cscVector = np.zeros(3, dtype=np.int32)
+        self.cscPassthrough = 0 # use matrix & vector directly
+
+
+    ## =============== overwrite methods  ===============
+    def dump(self, filename=None):
+        if filename == None or filename == "":
+            print(f"[{self.name}] Config parameters shown below:")
+            dumpdata = "".join([f"  - %s: %s\n" % item for item in self.__dict__.items()])
+            print(dumpdata)
+            return True
+
+        with open(filename, "w") as f:
+            data = {
+                "version": self.version,
+                "rand_seed": self.randSeed,
+                "cscEnable": self.cscEnable,
+                "cscCctCtrlEn": self.cscCctCtrlEn,
+                "cscBrightness": self.cscBrightness,
+                "cscHue": self.cscHue,
+                "cscContrast": self.cscContrast,
+                "cscSaturation": self.cscSaturation,
+                "cscRGain": self.cscRGain,
+                "cscGGain": self.cscGGain,
+                "cscBGain": self.cscBGain,
+                "cscROffset": self.cscROffset,
+                "cscGOffset": self.cscGOffset,
+                "cscBOffset": self.cscBOffset,
+                "cscMatrix": self.cscMatrix,
+                "cscVector": self.cscVector,
+                "cscPassthrough": self.cscPassthrough,
+            }
+            nest_data = {"pq_tuning_param": {"csc": data}}
+            json.dump(nest_data, f, indent=4, ensure_ascii=False)
+            return True
+
+        return False
+
+    def load(self, filename):
+        # check config file validity
+        if not os.path.exists(filename):
+            print(f"[{self.name}] config file '{filename}' doesn't exist!")
+            return False
+        if not filename.endswith(".json"):
+            print(f"[{self.name}] config file '{filename}' is not a json file!")
+            return False
+
+        try:
+            with open(filename, "r") as f:
+                data = json.load(f)
+                if "pq_tuning_param" in data:
+                    print(f"[{self.name}] load config from pq_tuning_param.csc ...")
+                    data = data["pq_tuning_param"]["csc"]
+                self.cscEnable = data["cscEnable"]
+                self.cscCctCtrlEn = data["cscCctCtrlEn"] if "cscCctCtrlEn" in data else 0
+                self.cscBrightness = data["cscBrightness"]
+                self.cscHue = data["cscHue"]
+                self.cscContrast = data["cscContrast"]
+                self.cscSaturation = data["cscSaturation"]
+                self.cscRGain = data["cscRGain"]
+                self.cscGGain = data["cscGGain"]
+                self.cscBGain = data["cscBGain"]
+                self.cscROffset = data["cscROffset"]
+                self.cscGOffset = data["cscGOffset"]
+                self.cscBOffset = data["cscBOffset"]
+                self.cscMatrix = data["cscMatrix"]
+                self.cscVector = data["cscVector"] if "cscVector" in data else [0, 0, 0]
+                self.cscPassthrough = data["cscPassthrough"] if "cscPassthrough" in data else 0
+                self.version = data["version"] if "version" in data else "unknown"
+                self.randSeed = data["rand_seed"] if "rand_seed" in data else -1
+                return True
+        except Exception as e:
+            print(f"[{self.name}] load config file '{filename}' failed: {e}")
+            return False
+
+    def check(self):
+        # TODO
+        self.valid = True
+        return self.valid
+
+    def gen(self, seed: int = 114514):
+        ## set random seed
+        if seed == None:
+            seed = self.randSeed + 1  # increase rand seed if no argument in
+        random.seed(seed)
+
+        self.randSeed = seed
+        self.version = f"{self.name.lower()}_config_rk3572_random_seed_{seed}"
+
+        self.cscEnable = int(random.randint(0, 9) > 0)  # %90 be ON
+        self.cscCctCtrlEn = 0 # always 0 for now
+        self.cscBrightness = random.randint(0, 511)
+        self.cscHue = random.randint(0, 511)
+        self.cscContrast = random.randint(0, 511)
+        self.cscSaturation = random.randint(0, 511)
+        self.cscRGain = random.randint(0, 511)
+        self.cscGGain = random.randint(0, 511)
+        self.cscBGain = random.randint(0, 511)
+        self.cscROffset = random.randint(0, 511)
+        self.cscGOffset = random.randint(0, 511)
+        self.cscBOffset = random.randint(0, 511)
+        self.cscMatrix = np.random.randint(-2**15, 2**15-1, size=(3, 3), dtype=np.int16) # s16
+        self.cscVector = np.random.randint(-2**31, 2**31-1, size=3, dtype=np.int32) # s32
+        self.cscPassthrough = 1 # 100% use matrix & vector directly for now!
+        return seed
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: %s load <config_json_file>" % sys.argv[0])
+        print("Usage: %s gen <rand_seed>" % sys.argv[0])
+        exit(-1)
+
+    config = CscConfig()
+    if sys.argv[1] == "gen":
+        seed = config.gen(int(sys.argv[2]))
+        load_ok = True
+    elif sys.argv[1] == "load":
+        load_ok = config.load(sys.argv[2])
+        if not load_ok:
+            exit(-1)
+    else:
+        print("Usage: %s load <config_json_file>" % sys.argv[0])
+        print("Usage: %s gen <config_json_file>" % sys.argv[0])
+        exit(-1)
+
+    check_ok = config.check()
+    print("load_ok: %s, check_ok: %s" % (load_ok, check_ok))
+    config.dump()
