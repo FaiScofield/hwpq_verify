028d0e25ee3ccafdafcf69c8bf6687be35885321
Vance <vance.wu@rock-chips.com>
2025-07-24 14:55:42 +0800

    增加 vdpp_3572 的 rxbb 文件。

--- script/cli_helper/__init__.py
+++ script/cli_helper/__init__.py
@@ -1,5 +1,7 @@
 from cli_helper.cli_helper_core import *
-from cli_helper.cli_helper_cfa import *
 from cli_helper.cli_helper_acm import *
 from cli_helper.cli_helper_csc import *
 from cli_helper.cli_helper_sharp import *
+
+
+from cli_helper.cli_helper_cfa import *
