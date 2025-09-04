637b9a9ce12b232165c28ef871dc17cd5bd9d0b4
Vance <vance.wu@rock-chips.com>
2025-09-04 14:20:10 +0800

    [csc/kernel] 增加vop版本宏定义，拷贝至内核代码。

--- src/kernel/rockchip_drm_vop.h
+++ src/kernel/rockchip_drm_vop.h
@@ -14,6 +14,48 @@
 #include "rockchip_drm_drv.h"
 #include "drm_color_mgmt.h" // drm_color_encoding
 
+/*
+ * major: IP major version, used for IP structure
+ * minor: big feature change under same structure
+ * build: RTL current SVN number
+ */
+#define VOP_VERSION(major, minor)	((major) << 8 | (minor))
+#define VOP_MAJOR(version)		((version) >> 8)
+#define VOP_MINOR(version)		((version) & 0xff)
+
+#define VOP_VERSION_RK3066		VOP_VERSION(2, 1)
+#define VOP_VERSION_RK3036		VOP_VERSION(2, 2)
+#define VOP_VERSION_RK3126		VOP_VERSION(2, 4)
+#define VOP_VERSION_PX30_LITE		VOP_VERSION(2, 5)
+#define VOP_VERSION_PX30_BIG		VOP_VERSION(2, 6)
+#define VOP_VERSION_RK3308		VOP_VERSION(2, 7)
+#define VOP_VERSION_RV1126		VOP_VERSION(2, 0xb)
+#define VOP_VERSION_RV1106		VOP_VERSION(2, 0xc)
+#define VOP_VERSION_RK3576_LITE		VOP_VERSION(2, 0xd)
+#define VOP_VERSION_RK3506		VOP_VERSION(2, 0xe)
+#define VOP_VERSION_RV1126B		VOP_VERSION(2, 0xf)
+#define VOP_VERSION_RK3288		VOP_VERSION(3, 0)
+#define VOP_VERSION_RK3288W		VOP_VERSION(3, 1)
+#define VOP_VERSION_RK3368		VOP_VERSION(3, 2)
+#define VOP_VERSION_RK3366		VOP_VERSION(3, 4)
+#define VOP_VERSION_RK3399_BIG		VOP_VERSION(3, 5)
+#define VOP_VERSION_RK3399_LITE		VOP_VERSION(3, 6)
+#define VOP_VERSION_RK3228		VOP_VERSION(3, 7)
+#define VOP_VERSION_RK3328		VOP_VERSION(3, 8)
+
+#define VOP2_VERSION(major, minor, build)	((major) << 24 | (minor) << 16 | (build))
+#define VOP2_MAJOR(version)		(((version) >> 24) & 0xff)
+#define VOP2_MINOR(version)		(((version) >> 16) & 0xff)
+#define VOP2_BUILD(version)		((version) & 0xffff)
+
+/* The new SOC VOP version is bigger than the old */
+#define VOP_VERSION_RK3568	VOP2_VERSION(0x40, 0x15, 0x8023)
+#define VOP_VERSION_RK3588	VOP2_VERSION(0x40, 0x17, 0x6786)
+#define VOP_VERSION_RK3528	VOP2_VERSION(0x50, 0x17, 0x1263)
+#define VOP_VERSION_RK3562	VOP2_VERSION(0x50, 0x17, 0x4350)
+#define VOP_VERSION_RK3576	VOP2_VERSION(0x50, 0x19, 0x9765)
+#define VOP_VERSION_RK3572	VOP2_VERSION(0x50, 0x20, 0x9372)
+
 
 struct post_csc_convert_mode
 {
