a25f5f1fb809a47d18f89a5972efc165a38f90f7
vance.wu <vance.wu@rock-chips.com>
2025-12-01 15:54:20 +0800

    fix[com] 对各图像格式的IO修正，补充缺失的8bit格式支持

--- src/utils/verify_com.c
+++ src/utils/verify_com.c
@@ -4,7 +4,7 @@
  * @author: vance.wu@rock-chips.com
  * @create: 2025-09-12
  * @history:
- *  2025-11-21 vance.wu: Add YUV422/420 8/10bit pixel formats support for IO.
+ *  2025-12-01 vance.wu: Add YUV422/420 8/10bit pixel formats support for IO.
  *  2025-10-23 vance.wu: Add more 8bit pixel formats support for IO.
  *  2025-10-12 vance.wu: Add 10bit-packed-YUV444 formats support for IO.
  *  2025-09-08 vance.wu: Add common macros & functions for commonly usage.
@@ -147,15 +147,16 @@ int image_read_to_planar(FILE *fp, void *p_buf, int frmidx, int w, int h, int fm
     }
 
     const int src_stride = common_verify_imgfmt_pitch_ratio(fmt) * w;
-    const bool has_alpha = false;
+    const bool keep_alpha = false; // ignore alpha channel here
+
     if (0 == ret) {
         if (depth == 8) {
             const int dst_stride = w * 1;
-            ret = imgcvt_to_planar_8bit_lsb(p_src, (uint8_t *)p_buf, w, h, src_stride, dst_stride, fmt, has_alpha);
+            ret = imgcvt_to_planar_8bit_lsb(p_src, (uint8_t *)p_buf, w, h, src_stride, dst_stride, fmt, keep_alpha);
         }
         else if (depth == 10) {
             const int dst_stride = w * 2;
-            ret = imgcvt_to_planar_10bit_lsb(p_src, (uint16_t *)p_buf, w, h, src_stride, dst_stride, fmt, has_alpha);
+            ret = imgcvt_to_planar_10bit_lsb(p_src, (uint16_t *)p_buf, w, h, src_stride, dst_stride, fmt, keep_alpha);
         }
         else {
             LOGE("%s: invalid target depth=%d !\n", __func__, depth);
@@ -184,8 +185,8 @@ int image_write_from_plannar(FILE *fp, void *p_buf, int frmidx, int w, int h, in
     const int frame_size = (w * h * bpp + 7) / 8;
     ushort *p_dst = (ushort *)calloc(frame_size, 1);
 
-    const bool has_alpha = false;
     const int dst_stride = common_verify_imgfmt_pitch_ratio(fmt) * w;
+    const bool has_alpha = false; // ignore alpha channel here
 
     if (depth == 8) {
         const int src_stride = w;
@@ -471,7 +472,7 @@ int imgcvt_unpack_10bit(uint8_t const *p_src, uint16_t *p_dst, int w, int h, int
 }
 
 int imgcvt_to_planar_10bit_lsb(uint8_t const *p_src, uint16_t *p_dst, int w, int h, int src_strd, int dst_strd,
-    int src_fmt, bool has_alpha)
+    int src_fmt, bool keep_alpha)
 {
     assert(p_src != (uint8_t *)p_dst);
     assert(dst_strd >= w * 2);
@@ -483,11 +484,15 @@ int imgcvt_to_planar_10bit_lsb(uint8_t const *p_src, uint16_t *p_dst, int w, int
     LOGD_f("src fmt: %d(%s), bpp: %d, frame_size: %d\n", src_fmt, common_verify_imgfmt_str(src_fmt), bpp, frame_size);
     LOGD_f("src_strd: %d, dst_strd: %d\n", src_strd, dst_strd);
 
+    int chnl_num = 3;
+    int chroma_hgt = (src_fmt % 10 >= YUV420P) ? h / 2 : h;
+    int chroma_wid = (src_fmt % 10 >= YUV422P) ? w / 2 : w;
+
     // dst planar addrs
     ushort *p_dst_yr = (ushort *)p_dst;
     ushort *p_dst_ug = (ushort *)((uint8_t *)p_dst + dst_strd * h);
     ushort *p_dst_vb = (ushort *)((uint8_t *)p_dst + dst_strd * h * 2);
-    ushort *p_dst_a = has_alpha ? (ushort *)((uint8_t *)p_dst + dst_strd * h * 3) : NULL;
+    ushort *p_dst_a = keep_alpha ? (ushort *)((uint8_t *)p_dst + dst_strd * h * 3) : NULL;
     // need to update p_dst_vb for YUV422P & YUV420P
     if (src_fmt % 10 == 6 || src_fmt % 10 == 7) {
         p_dst_vb = (ushort *)((uint8_t *)p_dst_ug + dst_strd / 2 * h);
@@ -499,31 +504,18 @@ int imgcvt_to_planar_10bit_lsb(uint8_t const *p_src, uint16_t *p_dst, int w, int
 
     switch (src_fmt) {
     /* 8bit normal data to 10bit planar lsb data */
+    case RGBA8888: chnl_num = 4; // NO break here!
     case RGB888:
-    case YUV444I: {
-        assert(src_strd >= w * 3);
+    case YUV444I:  {
+        assert(src_strd >= w * chnl_num);
         for (int y = 0; y < h; y++) {
             for (int x = 0; x < w; x++) {
-                const int src_ofs = y * src_strd + x * 3;
+                const int src_ofs = y * src_strd + x * chnl_num;
                 const int dst_ofs = y * dst_strd / 2 + x;
                 p_dst_yr[dst_ofs] = p_src[src_ofs + 0] << 2;
                 p_dst_ug[dst_ofs] = p_src[src_ofs + 1] << 2;
                 p_dst_vb[dst_ofs] = p_src[src_ofs + 2] << 2;
-            }
-        }
-    } break;
-    case RGBA8888: {
-        assert(src_strd >= w * 4);
-        for (int y = 0; y < h; y++) {
-            for (int x = 0; x < w; x++) {
-                const int src_ofs = y * src_strd + x * 4;
-                const int dst_ofs = y * dst_strd / 2 + x;
-                p_dst_yr[dst_ofs] = p_src[src_ofs + 0] << 2;
-                p_dst_ug[dst_ofs] = p_src[src_ofs + 1] << 2;
-                p_dst_vb[dst_ofs] = p_src[src_ofs + 2] << 2;
-                if (has_alpha) {
-                    p_dst_a[dst_ofs] = p_src[src_ofs + 3] << 2;
-                }
+                // ignore alpha channel here
             }
         }
     } break;
@@ -616,16 +608,8 @@ int imgcvt_to_planar_10bit_lsb(uint8_t const *p_src, uint16_t *p_dst, int w, int
     case YUV422P_10LSB:
     case YUV420P_10LSB:   {
         assert(src_strd >= w * 2);
-        if (src_strd == dst_strd) {
-            memcpy(p_dst, p_src, frame_size);
-        }
-        else {
-            const int buf_hgt = src_fmt == YUV420P_10LSB ? h * 3 / 2 : (src_fmt == YUV422P_10LSB ? h * 2 : h * 3);
-            const int copy_size = MIN(src_strd, dst_strd);
-            for (int y = 0; y < buf_hgt; y++) {
-                memcpy((uchar *)p_dst + y * dst_strd, p_src + y * src_strd, copy_size);
-            }
-        }
+        assert(src_strd == dst_strd);
+        memcpy(p_dst, p_src, frame_size);
     } break;
     case YUV444SP_10LSB: {
         assert(src_strd >= w * 2);
@@ -721,7 +705,7 @@ int imgcvt_to_planar_10bit_lsb(uint8_t const *p_src, uint16_t *p_dst, int w, int
                 p_dst_yr[dst_ofs] = (rgba >> 0) & 0x3ff;
                 p_dst_ug[dst_ofs] = (rgba >> 10) & 0x3ff;
                 p_dst_vb[dst_ofs] = (rgba >> 20) & 0x3ff;
-                if (has_alpha) {
+                if (keep_alpha) {
                     p_dst_a[dst_ofs] = (rgba >> 30) << 8; // to 10bit alpha
                 }
                 // unpack_data_1010102(rgba, )
@@ -873,16 +857,8 @@ int imgcvt_from_planar_10bit_lsb(uint16_t const *p_src, uint8_t *p_dst, int w, i
     case YUV422P_10LSB:
     case YUV420P_10LSB:   {
         assert(dst_strd >= w * 2);
-        if (src_strd == dst_strd) {
-            memcpy(p_dst, p_src, frame_size);
-        }
-        else {
-            const int buf_hgt = dst_fmt == YUV420P_10LSB ? h * 3 / 2 : (dst_fmt == YUV422P_10LSB ? h * 2 : h * 3);
-            const int copy_size = MIN(src_strd, dst_strd);
-            for (int y = 0; y < buf_hgt; y++) {
-                memcpy(p_dst + y * dst_strd, (uchar *)p_src + y * src_strd, copy_size);
-            }
-        }
+        assert(src_strd == dst_strd);
+        memcpy(p_dst, p_src, frame_size);
     } break;
     case YUV444SP_10LSB: {
         assert(dst_strd >= w * 2);
@@ -1082,7 +1058,7 @@ int imgcvt_from_planar_10bit_lsb(uint16_t const *p_src, uint8_t *p_dst, int w, i
 }
 
 int imgcvt_to_planar_8bit_lsb(uint8_t const *p_src, uint8_t *p_dst, int w, int h, int src_strd, int dst_strd,
-    int src_fmt, bool has_alpha)
+    int src_fmt, bool keep_alpha)
 {
     assert(p_src != p_dst);
     assert(dst_strd >= w * 1);
@@ -1094,41 +1070,31 @@ int imgcvt_to_planar_8bit_lsb(uint8_t const *p_src, uint8_t *p_dst, int w, int h
     LOGD("src fmt: %d(%s), bpp: %d, frame_size: %d\n", src_fmt, common_verify_imgfmt_str(src_fmt), bpp, frame_size);
     LOGD("src_strd: %d, dst_strd: %d\n", src_strd, dst_strd);
 
-    int chroma_hgt = (src_fmt == YUV420P || src_fmt == YUV420SP) ? h / 2 : h;
+    int chnl_num = 3;
+    int chroma_hgt = (src_fmt % 10 >= YUV420P) ? h / 2 : h;
+    int chroma_wid = (src_fmt % 10 >= YUV422P) ? w / 2 : w;
 
     uint8_t *p_dst_yr = p_dst;
     uint8_t *p_dst_ug = p_dst + dst_strd * h;
     uint8_t *p_dst_vb = p_dst + dst_strd * h * 2;
-    uint8_t *p_dst_a = has_alpha ? (p_dst + dst_strd * h * 3) : NULL;
+    uint8_t *p_dst_a = keep_alpha ? (p_dst + dst_strd * h * 3) : NULL;
     LOGT("%s: dst u/v offset: %td / %td\n", __func__, p_dst_ug - p_dst_yr, p_dst_vb - p_dst_yr);
 
     switch (src_fmt) {
     /* 8bit normal data to 8bit planar lsb data */
+    case RGBA8888: chnl_num = 4; // NO break here!
     case RGB888:
-    case YUV444I: {
-        assert(src_strd >= w * 3);
+    case YUV444I:  {
+        assert(src_strd >= w * chnl_num);
         for (int y = 0; y < h; y++) {
             for (int x = 0; x < w; x++) {
-                const int src_ofs = y * src_strd + x * 3;
+                const int src_ofs = y * src_strd + x * chnl_num;
                 const int dst_ofs = y * dst_strd + x;
                 p_dst_yr[dst_ofs] = p_src[src_ofs + 0];
                 p_dst_ug[dst_ofs] = p_src[src_ofs + 1];
                 p_dst_vb[dst_ofs] = p_src[src_ofs + 2];
-            }
-        }
-    } break;
-    case RGBA8888: {
-        assert(src_strd >= w * 4);
-        for (int y = 0; y < h; y++) {
-            for (int x = 0; x < w; x++) {
-                const int src_ofs = y * src_strd + x * 4;
-                const int dst_ofs = y * dst_strd + x;
-                p_dst_yr[dst_ofs] = p_src[src_ofs + 0];
-                p_dst_ug[dst_ofs] = p_src[src_ofs + 1];
-                p_dst_vb[dst_ofs] = p_src[src_ofs + 2];
-                if (has_alpha) {
-                    p_dst_a[dst_ofs] = p_src[src_ofs + 3];
-                }
+                // ignore alpha channel here
+                // p_dst_a[dst_ofs] = p_src[src_ofs + 3];
             }
         }
     } break;
@@ -1137,16 +1103,8 @@ int imgcvt_to_planar_8bit_lsb(uint8_t const *p_src, uint8_t *p_dst, int w, int h
     case YUV422P:
     case YUV420P:    {
         assert(src_strd >= w * 1);
-        if (src_strd == dst_strd) {
-            memcpy(p_dst, p_src, frame_size);
-        }
-        else {
-            const int buf_hgt = src_fmt == YUV420P ? h * 3 / 2 : (src_fmt == YUV422P ? h * 2 : h * 3);
-            const int copy_size = MIN(src_strd, dst_strd);
-            for (int y = 0; y < buf_hgt; y++) {
-                memcpy(p_dst + y * dst_strd, p_src + y * src_strd, copy_size);
-            }
-        }
+        assert(src_strd == dst_strd);
+        memcpy(p_dst, p_src, frame_size);
     } break;
     case YUV444SP: {
         assert(src_strd >= w * 1);
@@ -1166,20 +1124,13 @@ int imgcvt_to_planar_8bit_lsb(uint8_t const *p_src, uint8_t *p_dst, int w, int h
     case YUV422SP:
     case YUV420SP: {
         assert(src_strd >= w * 1);
+        assert(src_strd == dst_strd);
         const uchar *p_src_y = p_src;
         const uchar *p_src_c = p_src + src_strd * h;
-        if (src_strd == dst_strd) {
-            memcpy(p_dst_yr, p_src_y, src_strd * h);
-        }
-        else {
-            const int copy_size = MIN(src_strd, dst_strd);
-            for (int y = 0; y < h; y++) {
-                memcpy(p_dst_yr + y * dst_strd, p_src_y + y * src_strd, copy_size);
-            }
-        }
+        memcpy(p_dst_yr, p_src_y, src_strd * h);
         p_dst_vb = p_dst_ug + dst_strd * h / (src_fmt == YUV422SP ? 2 : 4); // update dst_v;
         for (int y = 0; y < chroma_hgt; y++) {
-            for (int x = 0; x < w / 2; x++) {
+            for (int x = 0; x < chroma_wid; x++) {
                 const int src_ofs_c = y * src_strd + x * 2;
                 const int dst_ofs_c = y * dst_strd / 2 + x;
                 p_dst_ug[dst_ofs_c] = p_src_c[src_ofs_c + 0];
@@ -1205,7 +1156,9 @@ int imgcvt_from_planar_8bit_lsb(uint8_t const *p_src, uint8_t *p_dst, int w, int
     LOGD("dst fmt: %d(%s), bpp: %d, frame_size: %d\n", dst_fmt, common_verify_imgfmt_str(dst_fmt), bpp, frame_size);
     LOGD("src_strd: %d, dst_strd: %d\n", src_strd, dst_strd);
 
-    int chroma_hgt = (dst_fmt == YUV420P || dst_fmt == YUV420SP) ? h / 2 : h;
+    int chnl_num = 3;
+    int chroma_hgt = (dst_fmt % 10 >= YUV420P) ? h / 2 : h;
+    int chroma_wid = (dst_fmt % 10 >= YUV422P) ? w / 2 : w;
 
     const uint8_t *p_src_yr = p_src;
     const uint8_t *p_src_ug = p_src + src_strd * h;
@@ -1215,20 +1168,30 @@ int imgcvt_from_planar_8bit_lsb(uint8_t const *p_src, uint8_t *p_dst, int w, int
 
     switch (dst_fmt) {
     /* 8bit planar lsb data to 8bit normal data */
-    case YUV444P:
-    case YUV422P:
-    case YUV420P: {
-        assert(dst_strd >= w * 1);
-        if (src_strd == dst_strd) {
-            memcpy(p_dst, p_src, frame_size);
-        }
-        else {
-            const int buf_hgt = dst_fmt == YUV420P ? h * 3 / 2 : (dst_fmt == YUV422P ? h * 2 : h * 3);
-            const int copy_size = MIN(src_strd, dst_strd);
-            for (int y = 0; y < buf_hgt; y++) {
-                memcpy(p_dst + y * dst_strd, p_src + y * src_strd, copy_size);
+    case RGBA8888: chnl_num = 4; // NO break here!
+    case RGB888:
+    case YUV444I:  {
+        assert(dst_strd >= w * chnl_num);
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                const int src_ofs = y * src_strd + x;
+                const int dst_ofs = y * dst_strd + x * chnl_num;
+                p_dst[dst_ofs + 0] = p_src_yr[src_ofs];
+                p_dst[dst_ofs + 1] = p_src_ug[src_ofs];
+                p_dst[dst_ofs + 2] = p_src_vb[src_ofs];
+                if (chnl_num == 4) {
+                    p_dst[dst_ofs + 3] = 0xFF;
+                }
             }
         }
+    } break;
+    case RGB_PLANAR:
+    case YUV444P:
+    case YUV422P:
+    case YUV420P:    {
+        assert(dst_strd >= w * 1);
+        assert(src_strd == dst_strd);
+        memcpy(p_dst, p_src, frame_size);
     } break;
     case YUV444SP: {
         assert(dst_strd >= w * 1);
@@ -1237,7 +1200,7 @@ int imgcvt_from_planar_8bit_lsb(uint8_t const *p_src, uint8_t *p_dst, int w, int
         for (int y = 0; y < h; y++) {
             for (int x = 0; x < w; x++) {
                 const int dst_ofs_y = y * dst_strd + x;
-                const int dst_ofs_c = y * dst_strd + x * 2;
+                const int dst_ofs_c = y * dst_strd * 2 + x * 2;
                 const int src_ofs = y * src_strd + x;
                 p_dst_y[dst_ofs_y] = p_src_yr[src_ofs];
                 p_dst_c[dst_ofs_c + 0] = p_src_ug[src_ofs];
@@ -1248,20 +1211,13 @@ int imgcvt_from_planar_8bit_lsb(uint8_t const *p_src, uint8_t *p_dst, int w, int
     case YUV422SP:
     case YUV420SP: {
         assert(dst_strd >= w * 1);
+        assert(src_strd == dst_strd);
         uchar *p_dst_y = p_dst;
         uchar *p_dst_c = p_dst + dst_strd * h;
-        if (src_strd == dst_strd) {
-            memcpy(p_dst_y, p_src_yr, src_strd * h);
-        }
-        else {
-            const int copy_size = MIN(src_strd, dst_strd);
-            for (int y = 0; y < h; y++) {
-                memcpy(p_dst_y + y * dst_strd, p_src_yr + y * src_strd, copy_size);
-            }
-        }
+        memcpy(p_dst_y, p_src_yr, src_strd * h);
         p_src_vb = p_src_ug + src_strd * h / (dst_fmt == YUV422SP ? 2 : 4); // update src_v;
         for (int y = 0; y < chroma_hgt; y++) {
-            for (int x = 0; x < w / 2; x++) {
+            for (int x = 0; x < chroma_wid; x++) {
                 const int dst_ofs_c = y * dst_strd + x * 2;
                 const int src_ofs_c = y * src_strd / 2 + x;
                 p_dst_c[dst_ofs_c + 0] = p_src_ug[src_ofs_c];
