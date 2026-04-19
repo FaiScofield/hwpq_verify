415e7f784acafd708179545c7f851bc3e386f917
Vance <wfyash@163.com>
2026-04-19 23:22:35 +0800

    refactor(build): 简化Windows构建脚本并移除RK_SOC参数
    - 移除不再需要的RK_SOC参数和相关逻辑
    - 使用更准确的PROJECT_DIR变量替代硬编码路径
    - 更新编译命令输出路径以使用PROJECT_DIR

--- project/build_win32_mingw.cmd
+++ project/build_win32_mingw.cmd
@@ -1,10 +1,9 @@
 @echo off
-@echo usage: build_win32_mingw.cmd ^[Debug^|Release^] ^[RK_PLATFORM^]
+@echo usage: build_win32_mingw.cmd ^[Debug^|Release^]
 
 :: NOTE: change the mingw64 directory to your own. :: use a version such as: 'x86_64-8.1.0-release-posix-seh-rt_v6'
 :: You might check the target compiler by running `D:/mingw64/bin/gcc.exe -v` Target: x86_64-w64-mingw32
 set MINGW_DIR="D:/mingw64"
-SET RK_SOC="RK3572"
 set BUILD_TYPE=Debug
 
 :: parse command line arguments
@@ -12,28 +11,24 @@ if /i "%~1" == "Release" (
     set BUILD_TYPE=Release
     @echo get BUILD_TYPE from cmd: %~1
 )
-if NOT "%~2" == "" (
-    set RK_SOC=%~2
-    @echo get RK_SOC from cmd: %~2
-)
 @echo -------------------------------
 
-set PWD=%dp0
-set BUILD_DIR=build_win32_%BUILD_TYPE%
+set PROJECT_DIR=%~dp0\..\
+set BUILD_DIR=%~dp0\build_win32_%BUILD_TYPE%
+echo PROJECT_DIR: %PROJECT_DIR%
+echo BUILD_DIR: %BUILD_DIR%
 
-
-cmake -G "MinGW Makefiles" -H../ -B%BUILD_DIR% ^
+cmake -G "MinGW Makefiles" -H%PROJECT_DIR% -B%BUILD_DIR% ^
     -DCMAKE_MAKE_PROGRAM="%MINGW_DIR%/bin/mingw32-make.exe" ^
     -DCMAKE_C_COMPILER="%MINGW_DIR%/bin/gcc.exe" ^
     -DCMAKE_CXX_COMPILER="%MINGW_DIR%/bin/g++.exe" ^
     -DCMAKE_VERBOSE_MAKEFILE=OFF ^
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
-    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
-    -DRK_SOC=%RK_SOC%
+    -DCMAKE_BUILD_TYPE="%BUILD_TYPE%"
 
 cmake --build %BUILD_DIR% --target install -j 6 --
 
 :: copy compile_commands.json to project root
-copy /Y %BUILD_DIR%\compile_commands.json ..\.vscode\
+copy /Y %BUILD_DIR%\compile_commands.json %PROJECT_DIR%\.vscode\
 
 @echo on
